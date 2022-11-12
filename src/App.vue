cba69a7e9c4e9f563f4ef1ec57b0d9b2cfab5c12
front-tanuki <115462132+front-tanuki@users.noreply.github.com>
2022-11-12 19:15:42 +0900

    vueでtodoリスト作成

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+  <div class="hello">
+    <h2>Todo app</h2>
+    <button style="submit" @click="deleteAll()">DeleteAll</button>
+
+    <h3>Enter todo</h3>
+    <p>Name:</p>
+    <input v-model="todo.name" />
+    <p>Description:</p>
+    <input v-model="todo.description" />
+    <p>Deadline:</p>
+    <input v-model="todo.deadline" />
+    <br /><br />
+    <button style="submit" @click="create">Create</button>
+    <hr />
+    <button style="submit" @click="read">Read</button>
+    <hr />
+    <div v-for="todo in todos" :key="todo.name">
+      <li>Name: {{ todo.name }}</li>
+      <li>Description: {{ todo.description }}</li>
+      <li>Deadline: {{ todo.deadline }}</li>
+      <button style="submit" @click="deleteTodo(todo)">Delete</button>
+      <hr />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorldVue from "./components/HelloWorld.vue";
+// import HelloWorldVue from "./components/HelloWorld.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld: HelloWorldVue,
+  data: () => ({
+    todos: [],
+    todo: {
+      name: "",
+      description: "",
+      deadline: "",
+    },
+    name: "todos",
+  }),
+  methods: {
+    read() {
+      const data = (this.todos = sessionStorage.getItem(this.name));
+      if (data) {
+        this.todos = JSON.parse(data);
+      }
+    },
+    create() {
+      this.todos.push(this.todo);
+      sessionStorage.setItem(this.name, JSON.stringify(this.todos));
+      this.todo = {
+        name: "",
+        description: "",
+        deadline: "",
+      };
+    },
+    deleteTodo(todo) {
+      // trueのアイテムだけtodos に戻す
+      this.todos = this.todos.filter((item) => item.name !== todo.name);
+      sessionStorage.setItem(this.name, JSON.stringify(this.todos));
+    },
+    deleteAll() {
+      sessionStorage.clear();
+      this.todos = [];
+    },
   },
 };
 </script>
